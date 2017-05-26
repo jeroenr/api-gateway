800c1d1da73726b3cd1f02aeb59a4ac8c44b9ccd
Jeroen Rosenberg <jeroen.rosenberg@cupenya.com>
2017-05-26 16:43:13 +0200

    Log error on list services

--- src/main/scala/com/github/cupenya/gateway/integration/ServiceDiscoveryAgent.scala
+++ src/main/scala/com/github/cupenya/gateway/integration/ServiceDiscoveryAgent.scala
@@ -49,7 +49,10 @@ class ServiceDiscoveryAgent[T <: ServiceUpdate](serviceDiscoverySource: ServiceD
 
   def watchServices(): Unit = {
     system.scheduler.schedule(SERVICE_POLLING_INTERVAL seconds, 5 seconds) {
-      serviceDiscoverySource.listServices.map(handleServiceUpdates)
+      serviceDiscoverySource.listServices.map(handleServiceUpdates).onFailure {
+        case t =>
+          log.error(s"Couldn't list services: ${t.getMessage}", t)
+      }
     }
   }
 
